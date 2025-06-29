3c87540e64e0db110f16abc692b2a3b0769a7da9
Gabitho <106996555+Gabitho@users.noreply.github.com>
2025-06-29 23:12:05 +0200

    Bro i was so dumb i didn't create any class in my main.java ! 10 minutes of my life lost :sob:

--- src/main/java/com/pvpheads/main.java
+++ src/main/java/com/pvpheads/main.java
@@ -5,16 +5,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.pvphead.listeners.PlayerDeathListener;
 import com.pvphead.recipes.DragonSwordRecipe;
 
-@Override
-public void onEnable() {
-    getLogger().info("Plugin PvpHeads activé !");
-    
-    // Enregistre les listeners
-    getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
+public class PvpHeadsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin PvpHeads activé !");
+        
+        // Enregistre les listeners
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
 
-    // Enregistre la commande /fakekill
-    this.getCommand("fakekill").setExecutor(new FakeKillCommand());
+        // Enregistre la commande /fakekill
+        this.getCommand("fakekill").setExecutor(new FakeKillCommand());
 
-    // Enregistre la recette de l'épée du dragon
-    DragonSwordRecipe.register(this);
+        // Enregistre la recette de l'épée du dragon
+        DragonSwordRecipe.register(this);
+    }
 }
