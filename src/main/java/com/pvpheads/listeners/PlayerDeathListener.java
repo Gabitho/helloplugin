66c0d9dcc903b0aae58d22c23c3134feac79c1d1
Gabitho <106996555+Gabitho@users.noreply.github.com>
2025-06-30 11:46:24 +0200

    patch of patch of patch of patch of patch

--- src/main/java/com/pvpheads/listeners/PlayerDeathListener.java
+++ src/main/java/com/pvpheads/listeners/PlayerDeathListener.java
@@ -1,7 +1,7 @@
 package com.pvpheads.listeners;
 
 import org.bukkit.Material;
-import org.bukkit.block.Action;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
