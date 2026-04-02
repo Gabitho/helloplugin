e33876ce622305a5e9e689603b4e300b4237b92e
Gabitho <gabriel.thorne.2011@gmail.com>
2026-04-02 15:38:22 +0400

    Placeholder for test

---
+++ src/main/java/com/pvpheads/commands/SpawnPassiveMobCommand.java
@@ -0,0 +1,42 @@
+package com.pvpheads.commands;
+
+import com.pvpheads.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import org.jetbrains.annotations.NotNull;
+
+//placeholder pour tester la commande de spawn de mob passif.
+
+public class SpawnPassiveMobCommand implements CommandExecutor {
+
+    private final Main plugin;
+
+    public SpawnPassiveMobCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, 
+                             @NotNull String label, @NotNull String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Seuls les joueurs peuvent utiliser cette commande.");
+            return true;
+        }
+
+        if (args.length == 0) {
+            player.sendMessage("Usage: /spawnPassiveMob <type>");
+            return true;
+        }
+
+        String type = args[0].toLowerCase();
+        
+        // TODO : ici on ajoutera le code pour créer le mob via le MobControl/Hitbox
+        player.sendMessage("Commande reçue pour spawn le mob passif : " + type);
+
+        return true;
+    }
+}
